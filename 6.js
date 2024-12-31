057b0f1847c1549636f1f6ba94602121714b62f4
Prasad Adhikary <162437568+Prasad-1004@users.noreply.github.com>
2024-12-31 16:25:51 +0000

    FD

--- 6.js
+++ 6.js
@@ -1,1 +1,13 @@
-//Write a function to determine whether a given string is a palindrome or not!!
+//Write a function to determine whether a given string is a palindrome or not!! A palindrome is a word, phrase , a number or other sequence of characters that reads the same forward and backward,ignoring spaces,punctuation and capitilization.
+
+const palindrome = (str) => {
+   str = str.toLowerCase().replace(/\W/g, "");
+   let r_str = str.split("").reverse().join("");
+   console.log(r_str);
+   return str === r_str ? true : false;
+}
+
+
+console.log(palindrome("A man, a plan, a canal, Panama"));
+console.log(palindrome("racecar"));
+console.log(palindrome("192"));
---
+++ 7.js
@@ -0,0 +1,9 @@
+// Write a function that takes an array of numbers as input and return the maximum number in the array
+
+const findMax = (arr) => {
+    arr = arr.toString().split(" ");
+    
+    console.log(arr);
+}
+
+console.log(findMax([1,5,9,6,8,7]));
