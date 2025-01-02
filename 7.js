aaccceb448a311641f3a8d78f40bf1ffc77df4c0
Prasad Adhikary <162437568+Prasad-1004@users.noreply.github.com>
2025-01-02 15:44:46 +0000

    ghg

--- 7.js
+++ 7.js
@@ -1,9 +1,7 @@
 // Write a function that takes an array of numbers as input and return the maximum number in the array
 
 const findMax = (arr) => {
-    arr = arr.toString().split(" ");
-    
-    console.log(arr);
+    return Math.max(...arr);
 }
 
 console.log(findMax([1,5,9,6,8,7]));
